981cb6df37e28dca3c30cb3e1c129d02f73abf6f
Sebastian <skov@skxv.dev>
2024-08-18 18:53:46 +0000

    Added custom red/yellow thresholds to all colors

--- src/components/charts/current.tsx
+++ src/components/charts/current.tsx
@@ -12,6 +12,17 @@ interface iCurrentProps {
 const Current = ({ data }: iCurrentProps) => {
   const [priceNow, setPriceNow] = React.useState<number | null>(null);
   const { resolvedTheme } = useTheme();
+  const [thresholds, setThresholds] = React.useState({ red: 0, yellow: 0 });
+
+  React.useEffect(() => {
+    const yellowThreshold = localStorage.getItem("yellowThreshold");
+    const redThreshold = localStorage.getItem("redThreshold");
+
+    if (yellowThreshold && redThreshold) {
+      // @ts-ignore
+      setThresholds({ red: redThreshold, yellow: yellowThreshold });
+    }
+  }, []);
 
   React.useEffect(() => {
     const currentHour = new Date().getHours();
@@ -27,8 +38,8 @@ const Current = ({ data }: iCurrentProps) => {
       value={
         <p
           className={cn("text-green-500", {
-            "text-yellow-500": priceNow! > 2.01,
-            "text-red-500": priceNow! > 2.5
+            "text-yellow-500": priceNow! > thresholds.yellow,
+            "text-red-500": priceNow! > thresholds.red
           })}
         >
           {priceNow ? `${priceNow} kr` : "Not found"}
@@ -41,8 +52,8 @@ const Current = ({ data }: iCurrentProps) => {
           className={cn(
             "flex items-center justify-center size-5 rounded-full bg-green-500 dark:text-black text-white",
             {
-              "bg-yellow-500": priceNow! > 2.01,
-              "bg-red-500": priceNow! > 2.5
+              "bg-yellow-500": priceNow! > thresholds.yellow,
+              "bg-red-500": priceNow! > thresholds.red
             }
           )}
           style={{
